1e964e410b86a9d4473f9cf87b92b13cecbe2c6e
Maxwell Kendall <maxwellkendall@c02nveghg3qc.users.bah.com>
2018-03-07 16:55:04 -0500

    adding secrets

---
+++ secrets/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  connection: 'mysql://tawgthbt4k5nr6pb:b1khf56stpcxpjdd@gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pajmna3j7yalbmz8',
+  uname: 'tawgthbt4k5nr6pb',
+  pwd: 'b1khf56stpcxpjdd',
+  port: '3306',
+  db: 'pajmna3j7yalbmz8',
+  googAuth: {
+    clientID: '813863696388-cn9h34fkjn7tc5drnedhh05l0hdjc8vr.apps.googleusercontent.com',
+    clientSecret: 'R1xqe2STVeaZvaMy34NKDTug',
+  },
+  facebookAuth: {
+    clientID: '1959157417735568',
+    clientSecret: '7521b83d0e4581260c7535ae05483739',
+  }
+};
